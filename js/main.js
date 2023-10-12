f6791ffd1f0fbd67b196453310e1df0f228bb2f3
Simona <simonasablinaite@gmail.com>
2023-10-12 11:13:53 +0300

    Comments

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//  Header start 
+//  Header end 
+
+//  Hero start 
+//  Hero end 
+
+//  About-us start 
+//  About-us end 
+
+//  Services start 
+//  Services end 
+
+//  Sponsores start 
+//  Sponsores end 
+
+//  Gallery start 
+//  Gallery end 
+
+//  FAQ start 
+//  FAQ end 
+
+//  Contacts start 
+//  Contacts end 
+
+//  Footer start 
+//  Footer end 
